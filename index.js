45a9578a76cffacc0543f74165295cad3c35b1a5
PhoenixCrimson <33698159+PhoenixCrimson@users.noreply.github.com>
2017-11-15 19:30:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ var fortunes = [
     "Perhaps later",
     "Most certainly"
 ];
-
+var killgifs = ["http://gifimage.net/wp-content/uploads/2017/08/strike-witches-gif-5.gif",
+"http://gifimage.net/wp-content/uploads/2017/08/strike-witches-gif-1.gif",
+"https://gifimage.net/wp-content/uploads/2017/08/strike-witches-gif-14.gif",
+"https://vignette.wikia.nocookie.net/powerlisting/images/b/b6/Explosion_Megumin.gif/revision/latest?cb=20160302023153",
+"https://gifimage.net/wp-content/uploads/2017/08/megumin-explosion-gif-5.gif",
+"https://1.bp.blogspot.com/-7CH5vfmZqTM/WIk1xdq7KoI/AAAAAAAAuA8/Chjj3YlP6kwm0hdd5iFwHJAgfwJz-8FOQCPcB/s1600/Omake%2BGif%2BAnime%2B-%2BKono%2BSubarashii%2BSekai%2Bni%2BShukufuku%2Bwo%2521%2B2%2B-%2BEpisode%2B3%2B-%2BMegumin%2BExplosion%2B3.gif"
+];
 
 var bot = new Discord.Client();
 
@@ -78,7 +84,23 @@ bot.on("message", function(message) {
             }   
             break;
 
-            
+        case "sourcecode":
+            message.channel.sendMessage("If you think it is fine I will show you my nudes:\nhttps://github.com/PhoenixCrimson/FC")
+        break;
+
+        case "kill":
+            let victim = message.mentions.users.first() || message.guild.members.get(args[0]);
+            if(!victim) {
+                message.channel.sendMessage("Use this when you have decided to mention the poor soul.");
+            }
+            else {
+                var embed = new Discord.RichEmbed() 
+                    .setDescription("Someone wishes you dead, " + args[1] )
+                    .setURL(killgifs[Math.floor(Math.random() * killgifs.length)])
+                    .setColor("dd1122")
+                message.channel.sendEmbed(embed)
+            }
+            break;
         default:
             message.channel.sendMessage("I do not know this, please teach me.")
             
